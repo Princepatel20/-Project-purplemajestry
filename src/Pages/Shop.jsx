d4bc6759d383110efea6b3a03cb6cc6cac935912
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-01-20 15:55:55 +0530

    Update Shop.jsx

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import "./Shop.css"
+
 
 const Shop = () => {
-  return (
-    <div>
-      Shop
+  return (<>
+  <div className="shop">
+    <div className="shop-title">
+    <h2>IN THE SPOTLIGHT</h2>
     </div>
+    <div className="1192_image">
+
+    </div>
+  </div>
+  </>
   )
 }
 
