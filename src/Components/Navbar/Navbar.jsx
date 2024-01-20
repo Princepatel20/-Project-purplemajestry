f09f5a7af1342ebf1fc16129b9c7e7540b523605
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-01-20 11:19:38 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
+
+  const [menu,setmenu] = useState("home");
+
   return (
     <div className='navbar'>
       <div className="nav-logo">
@@ -10,14 +14,16 @@ const Navbar = () => {
         <p>PurpleMajestry</p>
       </div>
       <ul className="nav-menu">
-        <li>Home</li>
-        <li>Shop</li>
-        <li>Menus</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li on onClick={()=>{setmenu("home")}}><Link to='/'>Home</Link>{menu==="home"?<hr/>:<></>}</li>
+        <li on onClick={()=>{setmenu("shop")}}><Link to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+        <li on onClick={()=>{setmenu("menus")}}><Link to='/'>Menus</Link>{menu==="menus"?<hr/>:<></>}</li>
+        <li on onClick={()=>{setmenu("about")}}><Link to='/'>About</Link>{menu==="About"?<hr/>:<></>}</li>
+        <li on onClick={()=>{setmenu("contact")}}><Link to='/'>Contact</Link>{menu==="contact"?<hr/>:<></>}</li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
+        {/* <img src={cart_icon} alt="" /> */}
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   )
