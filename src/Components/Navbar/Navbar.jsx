1a61a4480fa9aa126f11bcfefc898b81325bc917
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-01-20 15:55:07 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom';
 
+import logo from "../Assets/purplemajestry.png"
+// import cart_icon from "../Assets/cart_icon.png"
+
 
 const Navbar = () => {
 
@@ -10,19 +13,19 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="nav-logo">
-        {/* <img src={logo} alt="" /> */}
-        <p>PurpleMajestry</p>
+        <img src={logo} alt="Navbar" />
+        
       </div>
       <ul className="nav-menu">
-        <li on onClick={()=>{setmenu("home")}}><Link to='/'>Home</Link>{menu==="home"?<hr/>:<></>}</li>
-        <li on onClick={()=>{setmenu("shop")}}><Link to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
-        <li on onClick={()=>{setmenu("menus")}}><Link to='/'>Menus</Link>{menu==="menus"?<hr/>:<></>}</li>
-        <li on onClick={()=>{setmenu("about")}}><Link to='/'>About</Link>{menu==="About"?<hr/>:<></>}</li>
-        <li on onClick={()=>{setmenu("contact")}}><Link to='/'>Contact</Link>{menu==="contact"?<hr/>:<></>}</li>
+        <li on onClick={()=>{setmenu("home")}}><Link style={{ textDecoration: 'none' }} to='/'>Home</Link>{menu==="home"?<hr/>:<></>}</li>
+        <li on onClick={()=>{setmenu("shop")}}><Link style={{ textDecoration: 'none' }} to='/shop'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+        <li on onClick={()=>{setmenu("menus")}}><Link style={{ textDecoration: 'none' }} to='/menus'>Menus</Link>{menu==="menus"?<hr/>:<></>}</li>
+        <li on onClick={()=>{setmenu("about")}}><Link style={{ textDecoration: 'none' }} to='/about'>About</Link>{menu==="About"?<hr/>:<></>}</li>
+        <li on onClick={()=>{setmenu("contact")}}><Link style={{ textDecoration: 'none' }} to='/contact'>Contact</Link>{menu==="contact"?<hr/>:<></>}</li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
-        {/* <img src={cart_icon} alt="" /> */}
+        {/* <img src={cart_icon} alt="Navbar" /> */}
         <div className="nav-cart-count">0</div>
       </div>
     </div>
