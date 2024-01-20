afefda77fc6235852a4bb8a499dec8ac3bd16d92
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-01-20 11:17:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import LoginSignup from "./Pages/LoginSignup";
+import Hero from "./Components/Hero/Hero";
+import Shop from "../src/Pages/Shop";
+import Menu from "./Pages/Menu";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Login from "./Components/Login/Login";
+import Cart from "../src/Pages/Cart";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/menus" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route
+            path="/mens"
+            element={
+              <ShopCategory
+                category="men/>}/>
+      <Route path='/womens' element={<ShopCategory category="
+                women
+              />
+            }
+          />
+          <Route path="product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
