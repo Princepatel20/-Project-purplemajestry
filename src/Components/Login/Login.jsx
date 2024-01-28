18c2109761656dcd85582d2c0de4f491e2f45a2b
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-01-28 14:37:05 +0530

    Update Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './Login.css';
+import LoginSignup from '../../Pages/LoginSignup';
 
 export default function Login() {
   return (
     <div>
       Login
+      <LoginSignup />
     </div>
-  )
+  );
 }
